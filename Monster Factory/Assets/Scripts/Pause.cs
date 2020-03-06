235bbc9a9fccf5bfc13d2cab80e7fd71e3d9d4d0
Joel Robinson <44164513+JR325243@users.noreply.github.com>
2020-03-06 15:27:38 +0000

    Basic UI
    Implemented Basic UI for the following:
    
    Pause and options menus
    
    Contract, resource, lab, and raid panels
    
    The latter might need to lead to a full screen panel but that is an easy fix
    
    Adding additional panels will be inconvenient as I was not able to find an efficient way of opening and closing each panel

---
+++ Monster Factory/Assets/Scripts/Pause.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    bool open = false;
+    public void PauseButton()
+    {
+        if (!open)
+        {
+            gameObject.SetActive(true);
+            open = true;
+        }
+        else
+        {
+            gameObject.SetActive(false);
+            open = false;
+        }
+    }
+}
