81fe1dbc9408e7658025e0c7e77903a4d60edfd7
Belty118 <George.belt17@bathspa.ac.uk>
2020-03-11 19:29:46 +0000

    Added the raid map and basic interaction

--- Monster Factory/Assets/Scripts/Raiding.cs
+++ Monster Factory/Assets/Scripts/Raiding.cs
@@ -1,18 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
 
 public class Raiding : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Text nametext;
+    public Text populationtext;
+    public Text difficultytext;
+
+
+    private int settlementnumber;
+
+    public class settlement //class for a settlement
     {
-        
+        public string name;
+        public int population;
+        public int difficulty;
+        public int gold;
+
+        public int number;
     }
 
-    // Update is called once per frame
+    public settlement[] SettlementsArray; //delcare array for settlements info
+
+    public settlement burgville = new settlement();
+    public settlement cityville = new settlement();
+
+    void Start()
+    {
+        SettlementsArray = new settlement[2];
+
+        burgville.name = "Burgville";
+        burgville.population = 230;
+        burgville.difficulty = 10;
+        burgville.gold = 50;
+        burgville.number = 1;
+
+        cityville.name = "Cityville";
+        cityville.population = 560;
+        cityville.difficulty = 30;
+        cityville.gold = 1000;
+        cityville.number = 2;
+
+        SettlementsArray[0] = burgville;
+        SettlementsArray[1] = cityville;
+    }
+
+
     void Update()
     {
-        
+        string townname = EventSystem.current.currentSelectedGameObject.name;
+        print(townname);
+        int townnumber = Int32.Parse(townname);
+        print(townnumber);
+
+        nametext.text = SettlementsArray[townnumber].name.ToString();
+        populationtext.text = SettlementsArray[townnumber].population.ToString();
+        difficultytext.text = SettlementsArray[townnumber].difficulty.ToString();
+    }
+
+
+    public void townnamegetter()
+    {
+        string townname = EventSystem.current.currentSelectedGameObject.name;
     }
 }
