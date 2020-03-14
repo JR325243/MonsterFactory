b299faaf7366f31264c2ca81a7d7a32997ea407a
Joel Robinson <44164513+JR325243@users.noreply.github.com>
2020-03-14 15:49:44 +0000

    Added comments
    Added comments to make code readable
    
    tried to avoid making changes to the scene but apparently that's impossible

--- Monster Factory/Assets/Scripts/MenuOpen.cs
+++ Monster Factory/Assets/Scripts/MenuOpen.cs
@@ -7,12 +7,12 @@ using DentedPixel;
 public class MenuOpen : MonoBehaviour
 {
     [SerializeField] //making the below gameobjects visible in the inspector without having to make them public
-    GameObject contracts, lab, resources, raids, buttons;
+    GameObject contracts, lab, resources, raids;
     bool contractsOpen = false, labOpen = false, resourcesOpen = false, raidsOpen = false; // these bools are important for making sure only one panel is open at a time
 
     void Update()
     {
-
+        //allows keys 1 2 3 4 to open the relevant menus if needed
         if (Input.GetKeyUp("1")){
             Contracts();
         }
@@ -32,8 +32,8 @@ public class MenuOpen : MonoBehaviour
 
     public void Contracts() //seperate functions required for each panel, I'm sure there's a more effective way but I couldn't find it
     {
-        Open(contracts, contractsOpen);
-        contractsOpen = contractsOpen ? false : true;
+        Open(contracts, contractsOpen); //runs the open function with the relevant gameobject and bool
+        contractsOpen = contractsOpen ? false : true; //ternary operator toggles the bool value, bypassing an issue where the open function did not correctly set the bool value
     }
 
     public void Lab()
@@ -59,14 +59,14 @@ public class MenuOpen : MonoBehaviour
     {
         if (!menuOpen) //if the panel isn't open this code will open it
         {
-            menu.transform.SetAsLastSibling();
-            LeanTween.moveLocalX(menu, 710f, 2f).setEaseOutExpo();
-            StartCoroutine(ButtonStop(menu, 2f));
+            menu.transform.SetAsLastSibling(); //positions this UI element at the bottom of the parent object so it is drawn last and shown on top of the other buttons
+            LeanTween.moveLocalX(menu, 710f, 2f).setEaseOutExpo(); //uses the tween library to move the UI element into view with animation
+            StartCoroutine(ButtonStop(menu, 2f)); //runs the coroutine responsible for disabling button functionality while the UI moves - this prevents the player from bugging out the UI
         }
         else
         {
-            LeanTween.moveLocalX(menu, 1200f, 0.5f).setEaseOutQuad();
-            StartCoroutine(ButtonStop(menu, 0.5f));
+            LeanTween.moveLocalX(menu, 1200f, 0.5f).setEaseOutQuad(); // moves the UI back off the screen with a tween animation
+            StartCoroutine(ButtonStop(menu, 0.5f)); //suspends button functionality
             
         }
         
@@ -85,16 +85,10 @@ public class MenuOpen : MonoBehaviour
         m3 = false;
     }
 
-    void ButtonParent(GameObject parent)
-    {
-        buttons.transform.SetParent(parent.transform);
-        buttons.transform.localPosition = Vector3.zero;
-    }
-
     IEnumerator ButtonStop(GameObject menu, float time)
     {
-        menu.GetComponentInChildren<Button>().interactable = false;
-        yield return new WaitForSeconds(time);
-        menu.GetComponentInChildren<Button>().interactable = true;
+        menu.GetComponentInChildren<Button>().interactable = false; //disables button interactibility for any child objects in the menu
+        yield return new WaitForSeconds(time); // waits for a variable amount of time - meaning this function can be reused across the game
+        menu.GetComponentInChildren<Button>().interactable = true; //restores button function after the tweening has ended
     }
 }
--- Monster Factory/Assets/Scripts/Pause.cs
+++ Monster Factory/Assets/Scripts/Pause.cs
@@ -10,6 +10,7 @@ public class Pause : MonoBehaviour
 
     private void Start()
     {
+        //ensures all menus are correctly scaled to begin with
         menu.transform.localScale = Vector3.zero;
         pause.transform.localScale = Vector3.one;
         options.transform.localScale = Vector3.zero;
@@ -18,13 +19,13 @@ public class Pause : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Escape))
+        if (Input.GetKeyUp(KeyCode.Escape)) //allows escape button to be used to acces the menu
         {
             PauseButton();
         }
     }
 
-    public void OptionsButton()
+    public void OptionsButton() //placing the coroutine inside of a fuction is necessary for accessing the code from button components
     {
         StartCoroutine(Wait());
     }
@@ -34,13 +35,18 @@ public class Pause : MonoBehaviour
     {
         if (!pauseOpen)
         {
-            LeanTween.scale(menu, Vector3.one, 1f).setEaseInOutQuint();
+            LeanTween.scale(menu, Vector3.one, 1f).setEaseInOutQuint(); // resizes the element from 0,0,0 to 1,1,1 with animation
             pauseOpen = true;
         }
         else
         {
-            LeanTween.scale(menu, Vector3.zero, 1f).setEaseInOutQuint();
+            LeanTween.scale(menu, Vector3.zero, 1f).setEaseInOutQuint(); //returns the element to 0,0,0
             pauseOpen = false;
+            if (optionsOpen) //if the player closes the pause menu while in the options section the game will return to the default pause menu when its next opened
+            {
+                StartCoroutine(Wait()); //runs the open options coroutine
+            }
+            
         }
     }
 
@@ -48,13 +54,14 @@ public class Pause : MonoBehaviour
     {
         if (!optionsOpen)
         {
-            LeanTween.scale(pause, Vector3.zero, 0.5f).setEaseInOutQuint();
-            yield return new WaitForSeconds(0.5f);
-            LeanTween.scale(options, Vector3.one, 0.5f).setEaseInOutQuint();
+            LeanTween.scale(pause, Vector3.zero, 0.5f).setEaseInOutQuint(); //shrinks the pause menu contents
+            yield return new WaitForSeconds(0.5f); //prevents the two tweens from overlapping in an ugly way
+            LeanTween.scale(options, Vector3.one, 0.5f).setEaseInOutQuint(); // embiggens the options menu contents
             optionsOpen = true;
         }
         else
         {
+            // exact opposite of the above
             LeanTween.scale(options, Vector3.zero, 0.5f).setEaseInOutQuint();
             yield return new WaitForSeconds(0.5f);
             LeanTween.scale(pause, Vector3.one, 0.5f).setEaseInOutQuint();
