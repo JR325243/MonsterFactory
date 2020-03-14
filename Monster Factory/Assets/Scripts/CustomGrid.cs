3aa0da577169b0a3a1d5281d2412234274ed75cf
Joel Robinson <44164513+JR325243@users.noreply.github.com>
2020-03-14 22:34:09 +0000

    Grid Placement
    Added grid placement to the game with the ability to spawn game objects into a location assigned by the mouse
    
    to do:
    
    find a way of deleting abandoned clones without the game throwing a load of errors
    
    make it so that the ghost instance that guides the player has a transparent material

---
+++ Monster Factory/Assets/Scripts/CustomGrid.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// https://www.youtube.com/watch?v=eUFwxK9Z9aw&list=PL-axpC8Nx3Wl8c5OWjge-1b0cqfX2LOcw&index=8
+
+[ExecuteInEditMode]
+public class CustomGrid : MonoBehaviour
+{
+    public GameObject target, ghost;
+    public float gridSize;
+    public LayerMask clickMask;
+    public static Vector3 mousePos;
+
+    Vector3 truePos;
+
+    private void Update()
+    {
+        mousePos = -Vector3.one;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100f, clickMask))
+        {
+            mousePos = hit.point;
+        }
+
+        target.transform.position = mousePos;
+    }
+
+    void LateUpdate()
+    {
+        truePos.x = Mathf.Floor(target.transform.position.x / gridSize) * gridSize; //sets the x value of the vector 3 to the grid position nearest the position of the target
+        truePos.y = 0.5f; //keeps the object fully above the floor
+        truePos.z = Mathf.Floor(target.transform.position.z / gridSize) * gridSize; //sets the z value of the vector 3 to the grid position nearest the position of the target
+
+        ghost.transform.position = truePos; //sets the position of the object after the correct grid position have been defined
+    }
+
+    
+}
---
+++ Monster Factory/Assets/Scripts/SpawnMachine.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnMachine : MonoBehaviour
+{
+    public GameObject prefab, container;
+    GameObject grid;
+    bool placementMode = false, occupied = false;
+
+    void Awake()
+    {
+        grid = GameObject.Find("Grid"); //located the grid object so that its components can be accessed
+        container = GameObject.Find("Machines");
+    }
+
+    public void EnablePlacement()
+    {
+        placementMode = true; //activating placement mode so that an object can be placed
+
+        //sets the ghost object to an instance of the prefab selected
+        grid.GetComponent<CustomGrid>().ghost = Instantiate(prefab, grid.GetComponent<CustomGrid>().ghost.transform.position, Quaternion.identity);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && placementMode)
+        {
+            for(int i = 0; i < GameManager.OccupiedSpaces.Count; i++)
+            {
+                if(GameManager.OccupiedSpaces[i] == grid.GetComponent<CustomGrid>().ghost.transform.position)
+                {
+                    occupied = true;
+                }
+            }
+
+            if (!occupied && grid.GetComponent<CustomGrid>().target.transform.position != -Vector3.one)
+            {
+                Spawn();
+                grid.GetComponent<CustomGrid>().ghost.transform.SetParent(container.transform);
+                grid.GetComponent<CustomGrid>().ghost.SetActive(false);
+                placementMode = false;
+            }
+            else
+            {
+                grid.GetComponent<CustomGrid>().ghost.transform.SetParent(container.transform);
+                grid.GetComponent<CustomGrid>().ghost.SetActive(false);
+
+                placementMode = false;
+                occupied = false;
+            }
+
+        }
+        else if ((Input.GetMouseButtonDown(1) && placementMode))
+        {
+            grid.GetComponent<CustomGrid>().ghost.transform.SetParent(container.transform);
+            grid.GetComponent<CustomGrid>().ghost.SetActive(false);
+
+            placementMode = false;
+            occupied = false;
+        }
+    }
+
+    public void Spawn()
+    {
+        Instantiate(prefab, grid.GetComponent<CustomGrid>().ghost.transform.position, Quaternion.identity, container.transform);
+        GameManager.FillSpace(grid.GetComponent<CustomGrid>().ghost.transform.position);
+
+        //insert code for resource removal here
+    }
+}
